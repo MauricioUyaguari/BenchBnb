fa3f336a55c5c1338e0c13a7131b118d0abd7b10
Mauricio Uyaguari <lmauricio12@gmail.com>
2017-12-31 19:53:21 -0500

    user auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+
+
+  def create
+    @user = Uder.new(users_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:username, :password)
+  end
+
+
+
+end
